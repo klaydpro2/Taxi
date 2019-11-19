e1b301a5da7ff5e046722e079dbf206449abd530
klaydpro2 <klaydpro@mail.ru>
2019-11-19 23:13:48 +0400

    first

---
+++ taxi-app/src/main/java/com/taxi/app/TaxiAppApplication.java
@@ -0,0 +1,13 @@
+package com.taxi.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaxiAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaxiAppApplication.class, args);
+    }
+
+}
